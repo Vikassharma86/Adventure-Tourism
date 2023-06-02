92197b27913e31dc8d8aa8661198efd0b4c45e25
vikassharma86 <vikassharma63922@gmail.com>
2023-06-02 18:57:58 +0530

    ui update

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -2,11 +2,236 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home-page'>
-      <h1 className="text-center display-1 fw-bold p-5">
-        Home Page
-      </h1>
+    
+      <>
+  <meta charSet="UTF-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+  <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+  <title>Material Design for Bootstrap</title>
+  {/* MDB icon */}
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+  {/* Font Awesome */}
+  <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
+  />
+  {/* Google Fonts Roboto */}
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+  />
+  {/* MDB */}
+  <link rel="stylesheet" href="css/mdb.min.css" />
+  {/* Start your project here*/}
+  <div id="preview" className="preview">
+    <div style={{ display: "none" }} />
+    <div style={{}}>
+      <div
+        data-draggable="true"
+        className=""
+        style={{ position: "relative" }}
+        draggable="false"
+      >
+        {/**/}
+        {/**/}
+        <section
+          draggable="false"
+          className="container pt-5"
+          data-v-271253ee=""
+        >
+          <section className="mb-10">
+            {" "}
+            <div
+              className="p-5 text-center bg-image"
+              style={{
+                height: 550,
+                backgroundSize: "cover",
+                backgroundPosition: "60% 70%",
+                backgroundColor: "transparent",
+                backgroundImage:
+                  'url("https://images.pexels.com/photos/4525148/pexels-photo-4525148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")'
+              }}
+              aria-controls="#picker-editor"
+            >
+              {" "}
+              <div
+                className="mask"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+              >
+                {" "}
+                <div className="d-flex justify-content-center align-items-center h-100">
+                  {" "}
+                  <div className="row d-flex justify-content-center">
+                    {" "}
+                    <div className="col-lg-8">
+                      {" "}
+                      <div className="text-white">
+                        {" "}
+                        <h2 className="mb-4 px-4 px-md-5 display-3 fw-bold ls-tight">
+                          {" "}
+                          <span>Are you ready</span> <br />{" "}
+                          <span className="">for an adventure?</span>{" "}
+                        </h2>{" "}
+                        <p className="text-white lead mb-0 px-4 px-md-5">
+                          <span
+                            style={{
+                              boxSizing: "inherit",
+                              margin: 0,
+                              padding: 0,
+                              border: 0,
+                              verticalAlign: "baseline 0px",
+                              fontWeight: 760,
+                              color: "rgb(0, 0, 0)",
+                              fontFamily:
+                                'Arial, "Helvetica Neue", Helvetica, sans-serif',
+                              textAlign: "start",
+                              backgroundColor: "rgb(255, 255, 255)"
+                            }}
+                          >
+                            If you are always trying to be normal, you will
+                            never know how amazing you can be
+                          </span>
+                        </p>{" "}
+                      </div>{" "}
+                    </div>{" "}
+                  </div>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
+          </section>
+        </section>
+        {/**/}
+      </div>
+      <div
+        data-draggable="true"
+        className=""
+        style={{ position: "relative" }}
+        draggable="false"
+      >
+        {/**/}
+        {/**/}
+        <section
+          draggable="false"
+          className="container pt-5"
+          data-v-271253ee=""
+        >
+          <section className="mb-10">
+            {" "}
+            <h2 className="fw-bold mb-5 text-center">Adevnture Travel News </h2>{" "}
+            <div className="row gx-lg-5 mb-5 align-items-center">
+              {" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <img
+                  src="https://images.pexels.com/photos/5834475/pexels-photo-5834475.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  className="w-100 shadow-4-strong rounded-4 mb-4"
+                  alt=""
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                />{" "}
+              </div>{" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <h3 className="fw-bold">Mega adventure tourism trails in India</h3>{" "}
+               
+                <p className="text-muted">
+                India is all set to launch two major adventure trails in 2023 – the Ganga Trail and Northwestern Himalayan Trail. There are many more trails that are in the pipeline such as Narmada Trail or Kaveri River Trail.
+                </p>{" "}
+                <p className="text-muted">The adventure tourism activities will be developed while keeping in mind the need for sustainability and opportunities for livelihood for local communities
+                </p>{" "}
+                <a
+                  className="btn   btn-primary"
+                  href="#"
+                  role="button"
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                >
+                  Read more
+                </a>{" "}
+              </div>{" "}
+            </div>{" "}
+            <div className="row gx-lg-5 mb-5 flex-lg-row-reverse align-items-center">
+              {" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <img
+                  src="https://img.traveltriangle.com/blog/wp-content/uploads/2019/12/Adventure-Maharashtra-Cover.jpg"
+                  className="w-100 shadow-4-strong rounded-4 mb-4"
+                  alt=""
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                />{" "}
+              </div>{" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <h3 className="fw-bold">Maharashtra set to revive tourism with caravan, adventure tourism policies</h3>{" "}
+                
+                <p className="text-muted">
+                The Maharashtra Tourism Development Corporation (MTDC) is all set to boost tourism in the state with more initiatives,
+                 including plans for caravan tourism and adventure tourism. The state government is soon expected to come up with 
+                 elaborate policies on the same. The development has come on the heels of last month's recorded surge in the number of 
+                 hotel bookings in the state; MTDC officials have previously stated that its resorts at a number of places in Maharashtra
+                  are completely booked for the upcoming Christmas and New Year season. 
+
+
+                </p>{" "}
+                <a
+                  className="btn   btn-primary"
+                  href="#"
+                  role="button"
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                >
+                  Read more
+                </a>{" "}
+              </div>{" "}
+            </div>{" "}
+            <div className="row gx-lg-5 mb-5 align-items-center">
+              {" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <img
+                  src="https://images.unsplash.com/photo-1593693411515-c20261bcad6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8a2VyYWxhJTIwdG91cmlzbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  className="w-100 shadow-4-strong rounded-4 mb-4"
+                  alt=""
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                />{" "}
+              </div>{" "}
+              <div className="col-md-6 mb-4 mb-md-0">
+                {" "}
+                <h3 className="fw-bold">Adventure tourism, the next big thing in Kerala</h3>{" "}
+                
+                <p className="text-muted">
+                Kerala is all set to promote activity-based tourism in a bid to promote tourism in this region.
+                Kerala Tourism Department is making effort in this aspect by focussing in two important sectors, i.e.,
+                MICE (Meetings, Incentives, Conferences and Exhibitions) tourism and adventure tourism.
+                </p>{" "}
+                <a
+                  className="btn   btn-primary"
+                  href="#"
+                  role="button"
+                  aria-controls="#picker-editor"
+                  draggable="false"
+                >
+                  Read more
+                </a>{" "}
+              </div>{" "}
+            </div>{" "}
+          </section>
+        </section>
+        {/**/}
+      </div>
     </div>
+  </div>
+  {/* End your project here*/}
+  {/* MDB */}
+  {/* Custom scripts */}
+</>
+
   )
 }
 
